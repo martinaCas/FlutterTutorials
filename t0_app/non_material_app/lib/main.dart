eb5b81c378ff307d8c1c0a8738fcda319d76b91d
martinaCas <martinacasal.web@gmail.com>
2022-05-19 10:11:37 +0200

    last com

--- t0_app/non_material_app/lib/main.dart
+++ t0_app/non_material_app/lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.blueGrey),
+      child: const Center(
+        child: Text(
+          'Ciao, sono un esempio di Non Material App',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 27,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
