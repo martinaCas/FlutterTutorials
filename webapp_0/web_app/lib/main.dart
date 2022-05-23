d3d39ee03e80b27f0af34f6f689d61406704bffe
martinaCas <martinacasal.web@gmail.com>
2022-05-23 12:19:57 +0200

    commit

--- webapp_0/web_app/lib/main.dart
+++ webapp_0/web_app/lib/main.dart
@@ -1,115 +1,197 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const LoginApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class LoginApp extends StatelessWidget {
+  const LoginApp();
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/': (context) => const LoginAppScreen(),
+        '/welcome': (context) => WelcomeScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginAppScreen extends StatelessWidget {
+  const LoginAppScreen();
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white60,
+      body: const Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: LoginAppForm(),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class LoginAppForm extends StatefulWidget {
+  const LoginAppForm();
 
-  void _incrementCounter() {
+  @override
+  _LoginAppFormState createState() => _LoginAppFormState();
+}
+
+class _LoginAppFormState extends State<LoginAppForm> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  double _formProgress = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      onChanged: _updateFormProgress,
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        //LinearProgressIndicator(value: _formProgress),
+        AnimatedProgressIndicator(value: _formProgress),
+        Text('Log in', style: Theme.of(context).textTheme.headline4),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            controller: _usernameController,
+            decoration: const InputDecoration(hintText: 'username'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            obscureText: true,
+            controller: _passwordController,
+            decoration: const InputDecoration(hintText: 'password'),
+          ),
+        ),
+        TextButton(
+          style: ButtonStyle(
+            foregroundColor:
+                MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+              return states.contains(MaterialState.disabled)
+                  ? null
+                  : Colors.white;
+            }),
+            backgroundColor:
+                MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+              return states.contains(MaterialState.disabled)
+                  ? null
+                  : Colors.cyan[800];
+            }),
+          ),
+          onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
+          child: const Text('Log in'),
+        ),
+      ]),
+    );
+  }
+
+//il seguente metodo serve per tenere traccia dello stato dell'utente nell'inserimento dei dati nel form
+//se l'utente non compila i campi, il bottone non è attivo
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [_usernameController, _passwordController];
+
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _formProgress = progress;
     });
+  }
+
+//collegato a onPressed del bottone
+  void _showWelcomeScreen() {
+    Navigator.of(context).pushNamed('/welcome');
+  }
+}
+
+//classe per schermata di benvenuto
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Benvenuto!', style: Theme.of(context).textTheme.headline2),
+      ),
+    );
+  }
+}
+
+//class per animazione
+class AnimatedProgressIndicator extends StatefulWidget {
+  final double value;
+
+  const AnimatedProgressIndicator({
+    required this.value,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AnimatedProgressIndicatorState();
+  }
+}
+
+class _AnimatedProgressIndicatorState extends State<AnimatedProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _colorAnim;
+  late Animation<double> _curveAnim;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        duration: Duration(milliseconds: 1200), vsync: this);
+
+    final colorTween = TweenSequence([
+      TweenSequenceItem(
+        tween: ColorTween(begin: Colors.red, end: Colors.orange),
+        weight: 1,
+      ),
+      TweenSequenceItem(
+        tween: ColorTween(begin: Colors.orange, end: Colors.yellow),
+        weight: 1,
+      ),
+      TweenSequenceItem(
+        tween: ColorTween(begin: Colors.yellow, end: Colors.green),
+        weight: 1,
+      ),
+    ]);
+
+    _colorAnim = _controller.drive(colorTween);
+    _curveAnim = _controller.drive(CurveTween(curve: Curves.easeIn));
+  }
+
+  @override
+  didUpdateWidget(oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _controller.animateTo(widget.value);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) => LinearProgressIndicator(
+              value: _curveAnim.value,
+              valueColor: _colorAnim,
+              backgroundColor: _colorAnim.value?.withOpacity(0.4),
+            ));
   }
 }
